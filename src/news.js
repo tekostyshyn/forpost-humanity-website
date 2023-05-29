cc24b4a21feb5d7c90f93716e6486a438bfa40a5
tekostyshyn <tekostyshyn@gmail.com>
2023-05-29 13:42:10 +0300

    fixed bug with image

--- src/news.js
+++ src/news.js
@@ -1,16 +1,17 @@
+import pic1 from './images/post_1.jpeg';
+import pic2 from './images/post_2.jpeg';
+
 export const newsUa = [
   {
     description:
       'Сьогодні, 9 травня 2023 року, ми вперше святкуємо День Європи з нашими західними сусідами. У цій війні обов’язково переможуть демократія, європейські цінності, свобода, людяність. Колись ми перемогли фашизм, а тепер переможемо рашизм. Слава Україні! Хай живе Європа!',
-    imageUrl:
-      'https://scontent.fiev25-2.fna.fbcdn.net/v/t39.30808-6/345594833_633550221961378_7708602762209786280_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=2Pi7MzR_CXYAX_jz5wU&_nc_ht=scontent.fiev25-2.fna&oh=00_AfCCnRaTwGJE4_bkycfEWTE7kK5KU-t_Kg9nsyZvUl1TNQ&oe=64740272',
+    imageUrl: pic1,
     date: 'Опубліковано: 09 травня 2023 року',
   },
   {
     description:
       'Сьогодні 438-й день від початку повномасштабної війни, яку росія розпочала проти України. Це довгий час, не дивно, що наш організм, основна біологічна мета якого – вижити, намагається адаптуватися, забути негативний досвід. Війна вразила і травмувала всіх нас. Це нормально, що навіть у такій ситуації ми намагаємося працювати, відпочивати, жити нормальним життя. Але ми не маємо права забувати про людей, які віддали своє життя за нас, за свободу, за Україну. Про людей, які постраждали від війни і потребують нашої допомоги. Не забуваймо залишатися людьми, підтримувати інших. Адже людяність врятує людство.',
-    imageUrl:
-      'https://scontent.fiev25-2.fna.fbcdn.net/v/t39.30808-6/345617322_1673499783112063_7966802930833588445_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=oBj4JOatI4YAX8LuL1T&_nc_ht=scontent.fiev25-2.fna&oh=00_AfDWckFe_3IMjE4fktZ1nDZHa1wISWzFSgc8eqoXksbv8Q&oe=64725484',
+    imageUrl: pic2,
     date: 'Опубліковано: 07 травня 2023 року',
   },
 ];
@@ -19,15 +20,12 @@ export const newsEn = [
   {
     description:
       'Today, on 9th of May 2023, for the first time we are celebrating the Day of Europe with our Western neighbours. Democracy, European values, Freedom, Humanity will definitely win in this war.  Once they have defeated fascism, and now they will defeat rashism. Glory to Ukraine! Long live Europe!',
-    imageUrl:
-      'https://scontent.fiev25-2.fna.fbcdn.net/v/t39.30808-6/345594833_633550221961378_7708602762209786280_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=2Pi7MzR_CXYAX_jz5wU&_nc_ht=scontent.fiev25-2.fna&oh=00_AfCCnRaTwGJE4_bkycfEWTE7kK5KU-t_Kg9nsyZvUl1TNQ&oe=64740272',
-    date: 'Published: 09 of May 2023',
+      imageUrl: pic1,
+      date: 'Published: 09 of May 2023',
   },
   {
     description:
       'Today is the 438th day since the beginning of full-scaled war, which russia started against Ukraine. This is a long time, no wonder that our body, which main biological goal is to survive, tries to adapt, to forget negative experience. War has shocked and traumatized all of us. It is okay that even in such situation we are trying to leave a normal life, to work, to have vacation. But beside this we should not forget about people, who gave their lifes for us, for freedoom, for Ukraine. About people who suffered from the war and need our help. We should not forget to stay humans, to support others. Because humanity will save humanity.',
-    imageUrl:
-      'https://scontent.fiev25-2.fna.fbcdn.net/v/t39.30808-6/345617322_1673499783112063_7966802930833588445_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=oBj4JOatI4YAX8LuL1T&_nc_ht=scontent.fiev25-2.fna&oh=00_AfDWckFe_3IMjE4fktZ1nDZHa1wISWzFSgc8eqoXksbv8Q&oe=64725484',
-    date: 'Published: 07 of May 2023',
+      imageUrl: pic2,    date: 'Published: 07 of May 2023',
   },
 ];
