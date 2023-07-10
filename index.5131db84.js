fe77e342b8df8d3cfbef71b51635d85f52ff58e2
tekostyshyn <113828114+tekostyshyn@users.noreply.github.com>
2023-07-10 15:13:05 +0000

    Deploying to gh-pages from @ tekostyshyn/forpost-humanity-website@e2a400b5b385dc6be31752a017238dca6d1a2ade 🚀

---
+++ index.5131db84.js
@@ -0,0 +1,2 @@
+!function(){var a="fbclid";if(-1!==location.search.indexOf("fbclid=")){var e="";try{var c=new URL(location);c.searchParams.delete(a),e=c.href}catch(a){var l=new RegExp("[?&]fbclid=.*$");e=location.search.replace(l,""),e=location.pathname+e+location.hash}history.replaceState(null,"",e)}}();
+//# sourceMappingURL=index.5131db84.js.map
